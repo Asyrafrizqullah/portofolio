e8f0269e23a9e555c8f6b5e12043686c9114478c
Muh Asyraf Rizqullah <61863178+Asyrafrizqullah@users.noreply.github.com>
2023-10-29 22:51:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-const darkModeToggle = document.getElementById('dark-mode-toggle');
-const body = document.body;
+// script.js
 
-darkModeToggle.addEventListener('click', () => {
-    if (body.classList.contains('dark-mode')) {
-        body.classList.remove('dark-mode');
-    } else {
-        body.classList.add('dark-mode');
+// Efek smooth scroll untuk tautan di navbar
+$('nav a').on('click', function (event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+            window.location.hash = hash;
+        });
     }
+});
+
+// Efek parallax pada header
+$(window).scroll(function () {
+    const scrollTop = $(this).scrollTop();
+    $('.jumbotron').css('background-position-y', -(scrollTop / 3) + 'px');
+});
+
+// Efek hover pada gambar profil
+$('.img-fluid').hover(function () {
+    $(this).css('transform', 'scale(1.1)');
+}, function () {
+    $(this).css('transform', 'scale(1)');
 });
